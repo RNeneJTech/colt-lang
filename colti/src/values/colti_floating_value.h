89f139fe5f85258d9f0ac94d57f7e6117684453a
R533-Code <raphaeldibnehmepc@gmail.com>
2022-05-08 20:22:45 +0300

    Renamed file and added ColtiFloat

---
+++ colti/src/values/colti_floating_value.h
@@ -0,0 +1,10 @@
+#ifndef HG_COLTI_DOUBLE_VALUE
+#define HG_COLTI_DOUBLE_VALUE
+
+/// @brief Colti Double (8 bytes double precision float)
+typedef double ColtiDouble;
+
+/// @brief Colti Float (4 bytes single precision float)
+typedef float ColtiFloat;
+
+#endif //HG_COLTI_DOUBLE_VALUE
--- colti/src/values/double_value.h
+++
@@ -1,6 +0,0 @@
-#ifndef HG_COLTI_DOUBLE_VALUE
-#define HG_COLTI_DOUBLE_VALUE
-
-typedef double ColtiDouble;
-
-#endif //HG_COLTI_DOUBLE_VALUE
