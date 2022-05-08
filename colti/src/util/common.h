fc8e31e4bc96c719949ec39cdf1323bc9d801ba1
R533-Code <raphaeldibnehmepc@gmail.com>
2022-05-08 11:44:26 +0300

    Fixed commet and DO_IF_DEBUG_BUILD macro

--- colti/src/util/common.h
+++ colti/src/util/common.h
@@ -2,6 +2,7 @@
 #define HG_COLTI_COMMON
 
 #include <stdint.h>
+#include <inttypes.h>
 #include <stdbool.h>
 #include <stddef.h>
 
@@ -13,26 +14,26 @@
 #include "memory.h"
 
 #ifdef COLTI_DEBUG_BUILD
-	//Terminates 
+	//Terminates the program if 'cond' is not true
 	#define colti_assert(cond, error) do { \
 	if (!(cond)) { \
-		printf("\nError from file %s, at line %d:\nFunction: %s\nError: %s\n", __FILE__, __LINE__, __FUNCTION__, (error)); \
+		printf("\nAssertion failed from file %s, at line %d:\nFunction: %s\nError: %s\n", __FILE__, __LINE__, __FUNCTION__, (error)); \
 		getc(stdin); \
 		exit(1); \
 	} } while (0)
 	
 	//On debug builds we want to check for memory leaks and where they are coming from
 	#define safe_malloc(size)		checked_malloc(size)
-	#define safe_free(ptr)			free(ptr)
+	#define safe_free(ptr)			checked_free(ptr)
 	
 	//Rather than having to type #ifdef COLTI_DEBUG_BUILD
-	#define DO_IF_DEBUG_BUILD(what) do { (what); } while(0)
+	#define DO_IF_DEBUG_BUILD(what) do { what; } while(0)
 #else
 	#define colti_assert(cond, error)
 	
 	//On release builds we don't want overhead for allocating
 	#define safe_malloc(size)		checked_malloc(size)
-	#define safe_free(ptr)			free(ptr)
+	#define safe_free(ptr)			checked_free(ptr)
 
 	//Doesn't do anything
 	#define DO_IF_DEBUG_BUILD(what) do { (what); } while(0)
