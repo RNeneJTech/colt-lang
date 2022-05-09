bc6a6f95fe4524b5419ff7480b8a02f7af2f36be
R533-Code <raphaeldibnehmepc@gmail.com>
2022-05-09 17:57:33 +0300

    Added Colti typedefs for integers and bool

---
+++ colti/src/values/colti_integer_value.h
@@ -0,0 +1,37 @@
+#ifndef HG_COLTI_INTEGER_VALUE
+#define HG_COLTI_INTEGER_VALUE
+
+#include <stdint.h>
+#include <stdbool.h>
+
+/// @brief Byte representing a 2 state value 'true' (1) 'false' (0)
+typedef bool ColtiBool;
+
+
+/// @brief Unsigned 8 bit integer
+typedef uint8_t ColtiUI8;
+
+/// @brief Unsigned 16 bit integer
+typedef uint16_t ColtiUI16;
+
+/// @brief Unsigned 32 bit integer
+typedef uint32_t ColtiUI32;
+
+/// @brief Unsigned 64 bit integer
+typedef uint64_t ColtiUI64;
+
+
+/// @brief Signed 8 bit integer
+typedef int8_t ColtiI8;
+
+/// @brief Signed 16 bit integer
+typedef int16_t ColtiI16;
+
+/// @brief Signed 32 bit integer
+typedef int32_t ColtiI32;
+
+/// @brief Signed 64 bit integer
+typedef int64_t ColtiI64;
+
+
+#endif //HG_COLTI_INTEGER_VALUE
