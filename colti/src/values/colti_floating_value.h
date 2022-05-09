43b3f969aa1f92eaa5f1e4f982560cc66c76968f
R533-Code <raphaeldibnehmepc@gmail.com>
2022-05-09 17:57:16 +0300

    Fixed header guard na,e

--- colti/src/values/colti_floating_value.h
+++ colti/src/values/colti_floating_value.h
@@ -1,5 +1,5 @@
-#ifndef HG_COLTI_DOUBLE_VALUE
-#define HG_COLTI_DOUBLE_VALUE
+#ifndef HG_COLTI_FLOATING_VALUE
+#define HG_COLTI_FLOATING_VALUE
 
 /// @brief Colti Double (8 bytes double precision float)
 typedef double ColtiDouble;
@@ -7,4 +7,4 @@ typedef double ColtiDouble;
 /// @brief Colti Float (4 bytes single precision float)
 typedef float ColtiFloat;
 
-#endif //HG_COLTI_DOUBLE_VALUE
+#endif //HG_COLTI_FLOATING_VALUE
