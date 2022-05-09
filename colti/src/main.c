e53eaa81d5336cc711c426fedad798747537baaa
R533-Code <raphaeldibnehmepc@gmail.com>
2022-05-09 19:00:18 +0300

    Tested new code

--- colti/src/main.c
+++ colti/src/main.c
@@ -4,15 +4,21 @@
 int main(int argc, const char** argv)
 {
 	printf(CONSOLE_BACKGROUND_BRIGHT_MAGENTA CONSOLE_FOREGROUND_BLACK
-		"COLTI v%s on %s" CONSOLE_COLOR_RESET "\n", COLTI_VERSION_STRING, COLTI_OS_STRING);
+		"COLTI v%s on %s" CONSOLE_COLOR_RESET "\n", COLTI_VERSION_STRING, COLTI_OS_STRING);	
 	Chunk chunk;
 	ChunkInit(&chunk);
 	ChunkWriteOpCode(&chunk, OP_IMMEDIATE_QWORD);
 	QWORD qword = { .d = 0.2};	
 	ChunkWriteQWord(&chunk, qword);
-	ChunkWriteOpCode(&chunk, OP_PRINT_DOUBLE);
-	ChunkWriteOpCode(&chunk, OP_NEGATE_DOUBLE);
-	ChunkWriteOpCode(&chunk, OP_PRINT_DOUBLE);
+	ChunkWriteOpCode(&chunk, OP_PRINT);
+	ChunkWriteOpCode(&chunk, OPERAND_COLTI_DOUBLE);
+	
+	ChunkWriteOpCode(&chunk, OP_NEGATE);
+	ChunkWriteOpCode(&chunk, OPERAND_COLTI_DOUBLE);
+
+	ChunkWriteOpCode(&chunk, OP_PRINT);
+	ChunkWriteOpCode(&chunk, OPERAND_COLTI_DOUBLE);
+
 	ChunkWriteOpCode(&chunk, OP_RETURN);
 	ChunkDisassemble(&chunk, "Test1");
 	
