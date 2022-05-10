364a5e2d90cd8a5ba6a1258eed69e3747728caa5
R533-Code <raphaeldibnehmepc@gmail.com>
2022-05-10 16:50:39 +0300

    Added type operand enum

--- colti/src/byte-code/byte_code.h
+++ colti/src/byte-code/byte_code.h
@@ -1,6 +1,9 @@
 #ifndef HG_COLTI_BYTE_CODE
 #define HG_COLTI_BYTE_CODE
 
+#include "values/colti_floating_value.h"
+#include "values/colti_integer_value.h"
+
 /// @brief Represents an instruction to be executed by the VM
 typedef enum
 {
@@ -41,8 +44,30 @@ typedef enum
 	OP_RETURN,
 } OpCode;
 
+
+/// @brief Represents an operand type
+typedef enum
+{
+	COLTI_BOOL		= OPERAND_COLTI_BOOL,
+	COLTI_DOUBLE	= OPERAND_COLTI_DOUBLE,
+	COLTI_FLOAT		= OPERAND_COLTI_FLOAT,
+	
+	COLTI_INT8		= OPERAND_COLTI_I8,
+	COLTI_INT16		= OPERAND_COLTI_I16,
+	COLTI_INT32		= OPERAND_COLTI_I32,
+	COLTI_INT64		= OPERAND_COLTI_I64,
+	
+	COLTI_UINT8		= OPERAND_COLTI_UI8,
+	COLTI_UINT16	= OPERAND_COLTI_UI16,
+	COLTI_UINT32	= OPERAND_COLTI_UI32,
+	COLTI_UINT64	= OPERAND_COLTI_UI64,
+
+} OperandType;
+
 /**********************************
 BYTE-CODE RUNNING
 **********************************/
+
+
 
 #endif //HG_COLTI_BYTE_CODE
