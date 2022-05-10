48186b8a5a5ce22894eb34ab526e5195b8b73531
R533-Code <raphaeldibnehmepc@gmail.com>
2022-05-10 17:48:48 +0300

    Changed test code

--- colti/src/main.c
+++ colti/src/main.c
@@ -7,29 +7,29 @@ int main(int argc, const char** argv)
 		"COLTI v%s on %s" CONSOLE_COLOR_RESET "\n", COLTI_VERSION_STRING, COLTI_OS_STRING);
 	Chunk chunk;
 	ChunkInit(&chunk);
-	ChunkWriteOpCode(&chunk, OP_IMMEDIATE_DWORD);
-	DWORD qword = { .f = 1.5 };
-	ChunkWriteDWORD(&chunk, qword);
+	ChunkWriteOpCode(&chunk, OP_IMMEDIATE_QWORD);
+	QWORD qword = { .i64 = -15 };
+	ChunkWriteQWORD(&chunk, qword);
 
 	ChunkWriteOpCode(&chunk, OP_NEGATE);
-	ChunkWriteOperand(&chunk, COLTI_FLOAT);
+	ChunkWriteOperand(&chunk, COLTI_INT64);
 
 	ChunkWriteOpCode(&chunk, OP_PRINT);
-	ChunkWriteOperand(&chunk, COLTI_FLOAT);
+	ChunkWriteOperand(&chunk, COLTI_INT64);
 
 
-	ChunkWriteOpCode(&chunk, OP_IMMEDIATE_DWORD);
-	DWORD qword2 = { .f = 1.5 };
-	ChunkWriteDWORD(&chunk, qword2);
+	ChunkWriteOpCode(&chunk, OP_IMMEDIATE_QWORD);
+	QWORD qword2 = { .i64 = 15 };
+	ChunkWriteQWORD(&chunk, qword2);
 
 	ChunkWriteOpCode(&chunk, OP_PRINT);
-	ChunkWriteOperand(&chunk, COLTI_FLOAT);
+	ChunkWriteOperand(&chunk, COLTI_INT64);
 
 	ChunkWriteOpCode(&chunk, OP_DIVIDE);
-	ChunkWriteOperand(&chunk, COLTI_FLOAT);
+	ChunkWriteOperand(&chunk, COLTI_INT64);
 
 	ChunkWriteOpCode(&chunk, OP_PRINT);
-	ChunkWriteOperand(&chunk, COLTI_FLOAT);
+	ChunkWriteOperand(&chunk, COLTI_INT64);
 
 	ChunkWriteOpCode(&chunk, OP_RETURN);
 	ChunkDisassemble(&chunk, "Test1");
