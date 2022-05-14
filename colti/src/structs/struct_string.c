477fff95e48cb9811f7ca3726734fc6028abdbfa
R533-Code <raphaeldibnehmepc@gmail.com>
2022-05-14 19:20:32 +0300

    Added string class with functions

--- colti/src/structs/struct_string.c
+++ colti/src/structs/struct_string.c
@@ -16,3 +16,108 @@ void StringFree(String* str)
 	DO_IF_DEBUG_BUILD(str->size = 0);
 	DO_IF_DEBUG_BUILD(str->capacity = 0);
 }
+
+void StringPrint(const String* str)
+{
+	printf("%.*s", (int)str->size, str->ptr);
+}
+
+uint64_t StringCapacity(const String* str)
+{
+	return str->capacity;
+}
+
+uint64_t StringSize(const String* str)
+{
+	return str->size;
+}
+
+bool StringIsEmpty(const String* str)
+{
+	return str->size == 0;
+}
+
+bool StringIsStackAllocated(const String* str)
+{
+	return str->capacity == STRING_SMALL_BUFFER_OPTIMIZATION;
+}
+
+void StringReplaceAllChar(String* str, char character, char with)
+{
+	colti_assert(str->ptr != NULL, "Huge bug: a string's buffer was NULL!");
+	for (size_t i = 0; i < str->size; i++)
+	{
+		if (str->ptr[i] == character)
+			str->ptr[i] = with;
+	}
+}
+
+void StringReplaceAllString(String* str, const char* what, const char* with)
+{
+	colti_assert(str->ptr != NULL, "Huge bug: a string's buffer was NULL!");
+	colti_assert(false, "NOT IMPLEMENTED!");
+}
+
+void StringFill(String* str, char character)
+{
+	colti_assert(str->ptr != NULL, "Huge bug: a string's buffer was NULL!");
+	memset(str->ptr, character, str->size);
+}
+
+void StringReserve(String* str, size_t size)
+{
+	char* temp = (char*)safe_malloc(str->capacity += size);
+	memcpy(temp, str->ptr, str->size);
+
+	if (str->capacity != STRING_SMALL_BUFFER_OPTIMIZATION + size)
+		safe_free(str->ptr);
+	str->ptr = temp;
+}
+
+String StringGetLine()
+{
+	String str;
+	str.capacity = 1024;
+	str.size = 0;
+	str.ptr = safe_malloc(1024);
+	fgets(str.ptr, 1024, stdin);
+	return str;
+}
+
+String StringGetFileContent(const char* path)
+{
+	FILE* file = fopen(path, "rb"); //Read-binary mode
+	if (file == NULL)
+	{
+		fprintf(stderr, CONSOLE_FOREGROUND_BRIGHT_RED"Couldn't open the file '%s'!\n"CONSOLE_COLOR_RESET, path);
+		exit(3);
+	}
+	fseek(file, 0L, SEEK_END);
+	size_t file_size = ftell(file); //Get file size
+	
+	String str;
+	str.ptr = safe_malloc(str.capacity = file_size + 1);
+	str.size = str.capacity;
+	
+	rewind(file); //Go back to the beginning of the file
+	size_t bytes_read = fread(str.ptr, sizeof(char), file_size, file);
+	fclose(file);
+	if (bytes_read != file_size)
+	{
+		fprintf(stderr, CONSOLE_FOREGROUND_BRIGHT_RED"Couldn't read all the content of the file '%s'!\n"CONSOLE_COLOR_RESET, path);
+		exit(2);
+	}
+	str.ptr[str.size - 1] = '\0';
+	return str;
+}
+
+StringView StringToStringView(const String* str)
+{
+	StringView strv = { str->ptr, str->ptr + str->size };
+	return strv;
+}
+
+void StringViewPrint(const StringView strv)
+{
+	printf("%.*s", (int)(strv.end - strv.start), strv.start);
+}
--- colti/src/structs/struct_string.h
+++ colti/src/structs/struct_string.h
@@ -1,3 +1,12 @@
+/** @file struct_string.h
+* A String struct helper for dealing with strings.
+* The string struct uses a small-buffer optimization.
+* Common operations, like getting content from the file are a lot easier
+* using the provided functions.
+* This header also contains the StringView struct, which is a lightweight
+* struct representing a non-owning view over an array of characters.
+*/
+
 #ifndef HG_COLTI_STRUCT_STRING
 #define HG_COLTI_STRUCT_STRING
 
@@ -5,6 +14,7 @@
 
 #define STRING_SMALL_BUFFER_OPTIMIZATION 32
 
+/// @brief An owning array of characters that is always NUL terminated
 typedef struct
 {
 	uint64_t capacity;
@@ -22,11 +32,77 @@ void StringInit(String* str);
 /// @param str The string to modify
 void StringFree(String* str);
 
+/// @brief Prints the content of the string to stdout
+/// @param str The string to print
+void StringPrint(const String* str);
+
+/// @brief Returns the current capacity of a string
+/// @param str The string for which to check
+/// @return The capacity of the string
+uint64_t StringCapacity(const String* str);
+
+/// @brief Returns the size of a string
+/// @param str The string for which to check
+/// @return The size of the string
+uint64_t StringSize(const String* str);
+
+/// @brief Checks if a string is empty or not
+/// @param str True if empty
+bool StringIsEmpty(const String* str);
+
+/// @brief Checks if a string is using its stack-allocated buffer
+/// @param str The string for which to check
+bool StringIsStackAllocated(const String* str);
+
+/// @brief Replaces all instances of 'character' with 'with'
+/// @param str The string to modify
+/// @param character The character for which to search
+/// @param with The character to replace with
+void StringReplaceAllChar(String* str, char character, char with);
+
+/// @brief Replaces all instances of 'what' with 'with'
+/// @param str The string to modify
+/// @param what The string to search for
+/// @param with The string to replace with
+void StringReplaceAllString(String* str, const char* what, const char* with);
+
+/// @brief Fills a string with the specified character
+/// @param str The string to modify
+/// @param character The character with which to fill the string
+void StringFill(String* str, char character);
+
+/// @brief Reserves 'size' MORE bytes for the string capacity
+/// @param str The string to modify
+/// @param size The number of bytes to add to the capacity
+void StringReserve(String* str, size_t size);
+
+/// @brief Asks the user for input, and returns it as a string
+/// @return The string containing the input
+String StringGetLine();
+
+/// @brief Reads all the content of a file and writes to a string
+/// @param path The path to the file
+/// @return The string containing the file data
+String StringGetFileContent(const char* path);
+
 /// @brief A non-owning view over an array of characters
 typedef struct
 {
 	const char* start;
 	const char* end;
 } StringView;
+
+/// @brief Returns a string view over a string
+/// @param str The string from which to extract the view
+/// @return A view over the whole 'str'
+StringView StringToStringView(const String* str);
+
+/// @brief Prints a string view to stdout
+/// @param strv The view to print
+void StringViewPrint(const StringView strv);
+
+/*****************************************
+IMPLEMENTATION HELPERS
+*****************************************/
 
 #endif //HG_COLTI_STRUCT_STRING
