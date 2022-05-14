fa4185f7e0516de783cd3d9a73944b9ffbe9f885
R533-Code <raphaeldibnehmepc@gmail.com>
2022-05-14 19:21:23 +0300

    Added test code

--- colti/src/main.c
+++ colti/src/main.c
@@ -5,44 +5,23 @@ int main(int argc, const char** argv)
 {
 	printf(CONSOLE_BACKGROUND_BRIGHT_MAGENTA CONSOLE_FOREGROUND_BLACK
 		"COLTI v%s on %s" CONSOLE_COLOR_RESET "\n", COLTI_VERSION_STRING, COLTI_OS_STRING);
-	Chunk chunk;
-	ChunkInit(&chunk);
-	ChunkWriteOpCode(&chunk, OP_IMMEDIATE_QWORD);
-	QWORD qword = { .i64 = -15 };
-	ChunkWriteQWORD(&chunk, qword);
-
-	ChunkWriteOpCode(&chunk, OP_NEGATE);
-	ChunkWriteOperand(&chunk, COLTI_INT64);
-
-	ChunkWriteOpCode(&chunk, OP_PRINT);
-	ChunkWriteOperand(&chunk, COLTI_INT64);
-
-
-	ChunkWriteOpCode(&chunk, OP_IMMEDIATE_QWORD);
-	QWORD qword2 = { .i64 = 15 };
-	ChunkWriteQWORD(&chunk, qword2);
-
-	ChunkWriteOpCode(&chunk, OP_PRINT);
-	ChunkWriteOperand(&chunk, COLTI_INT64);
-
-	ChunkWriteOpCode(&chunk, OP_DIVIDE);
-	ChunkWriteOperand(&chunk, COLTI_INT64);
-
-	ChunkWriteOpCode(&chunk, OP_PRINT);
-	ChunkWriteOperand(&chunk, COLTI_INT64);
-
-	ChunkWriteOpCode(&chunk, OP_RETURN);
-	ChunkDisassemble(&chunk, "Test1");
 	
-	if (ChunkIsStackAllocated(&chunk))
-		printf("Chunk is stack-allocated!\n");
+	if (argc == 1)
+	{
+		printf("> ");
+		String line = StringGetLine();
+		printf("Line: %s", line.ptr);
+		StringFree(&line);
+	}
+	else if (argc == 2)
+	{
+		String file_content = StringGetFileContent(argv[1]);
+		printf("Line: %s", file_content.ptr);
+		StringFree(&file_content);
+	}
 	else
-		printf("Chunk is heap-allocated!\n");
-
-	StackVM vm;
-	StackVMInit(&vm);
-	StackVMRun(&vm, &chunk);
-
-	StackVMFree(&vm);
-	ChunkFree(&chunk);
+	{
+		printf("Usage: "CONSOLE_FOREGROUND_BRIGHT_BLUE"colti"CONSOLE_FOREGROUND_BRIGHT_MAGENTA" [path]"CONSOLE_COLOR_RESET);
+		exit(3);
+	}
 }
