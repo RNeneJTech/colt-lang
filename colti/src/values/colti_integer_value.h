93c32448de824f05eb6a727b3dcf60cf5f04a8a4
R533-Code <raphaeldibnehmepc@gmail.com>
2022-05-15 08:02:49 +0300

    Added comments for macro

--- colti/src/values/colti_integer_value.h
+++ colti/src/values/colti_integer_value.h
@@ -10,39 +10,48 @@
 
 /// @brief Byte representing a 2 state value 'true' (1) 'false' (0)
 typedef bool ColtiBool;
+/// @brief The operand representing a bool
 #define OPERAND_COLTI_BOOL 0
 
 /// @brief Unsigned 8 bit integer
 typedef uint8_t ColtiUI8;
+/// @brief The operand representing a unsigned 8-bit integer
 #define OPERAND_COLTI_UI8 1
 
 /// @brief Unsigned 16 bit integer
 typedef uint16_t ColtiUI16;
+/// @brief The operand representing a unsigned 16-bit integer
 #define OPERAND_COLTI_UI16 2
 
 /// @brief Unsigned 32 bit integer
 typedef uint32_t ColtiUI32;
+/// @brief The operand representing a unsigned 32-bit integer
 #define OPERAND_COLTI_UI32 3
 
 /// @brief Unsigned 64 bit integer
 typedef uint64_t ColtiUI64;
+/// @brief The operand representing a unsigned 64-bit integer
 #define OPERAND_COLTI_UI64 4
 
 
 /// @brief Signed 8 bit integer
 typedef int8_t ColtiI8;
+/// @brief The operand representing a signed 8-bit integer
 #define OPERAND_COLTI_I8 5
 
 /// @brief Signed 16 bit integer
 typedef int16_t ColtiI16;
-#define OPERAND_COLTI_I16 6 
+/// @brief The operand representing a signed 16-bit integer
+#define OPERAND_COLTI_I16 6
 
 /// @brief Signed 32 bit integer
 typedef int32_t ColtiI32;
+/// @brief The operand representing a signed 32-bit integer
 #define OPERAND_COLTI_I32 7
 
 /// @brief Signed 64 bit integer
 typedef int64_t ColtiI64;
+/// @brief The operand representing a signed 64-bit integer
 #define OPERAND_COLTI_I64 8
 
 
