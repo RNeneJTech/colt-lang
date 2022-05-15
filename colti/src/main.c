cc70cf1289d808232661e2199f9284dd9bfa68b7
R533-Code <raphaeldibnehmepc@gmail.com>
2022-05-15 20:02:03 +0300

    Removed ParseFree as now ParseResult no longer allocate heap memory

--- colti/src/main.c
+++ colti/src/main.c
@@ -3,7 +3,7 @@
 int main(int argc, const char** argv)
 {
 	ParseResult result = ParseArguments(argc, argv);
-	if (result.file_path == NULL)
+	if (result.file_path_in == NULL)
 	{
 		printf("> ");
 		String line = StringGetLine();		
@@ -13,5 +13,5 @@ int main(int argc, const char** argv)
 	{
 		String file_content = StringGetFileContent(argv[1]);
 		StringFree(&file_content);
-	}	
+	}
 }
