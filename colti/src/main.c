682a08817799b0ac441edf981a27be06b8513e9b
R533-Code <raphaeldibnehmepc@gmail.com>
2022-05-15 18:35:15 +0300

    Added argument parsing

--- colti/src/main.c
+++ colti/src/main.c
@@ -2,23 +2,16 @@
 
 int main(int argc, const char** argv)
 {
-	printf(CONSOLE_BACKGROUND_BRIGHT_MAGENTA CONSOLE_FOREGROUND_BLACK
-		"COLTI v%s on %s" CONSOLE_COLOR_RESET "\n", COLTI_VERSION_STRING, COLTI_OS_STRING);
-	
-	if (argc == 1)
+	ParseResult result = ParseArguments(argc, argv);
+	if (result.file_path == NULL)
 	{
 		printf("> ");
 		String line = StringGetLine();		
 		StringFree(&line);
 	}
-	else if (argc == 2)
+	else
 	{
 		String file_content = StringGetFileContent(argv[1]);
 		StringFree(&file_content);
-	}
-	else
-	{
-		printf("Usage: "CONSOLE_FOREGROUND_BRIGHT_BLUE"colti"CONSOLE_FOREGROUND_BRIGHT_MAGENTA" [path]"CONSOLE_COLOR_RESET);
-		exit(3);
 	}	
 }
