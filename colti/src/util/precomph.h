b716bc55ea8c3872df397f70cf5230cad3b5e863
R533-Code <raphaeldibnehmepc@gmail.com>
2022-05-16 14:51:33 +0300

    Added DUMP_MEMORY_LEAKS() which on Windows Debug build will dump memory leaks

--- colti/src/util/precomph.h
+++ colti/src/util/precomph.h
@@ -20,4 +20,14 @@
 #include "structs/struct_string.h"
 #include "util/parse_args.h"
 
+//DEBUGING UTILITIES
+#if defined(COLTI_WINDOWS) && defined(COLTI_DEBUG_BUILD)
+	#define _CRTDBG_MAP_ALLOC
+	#include <crtdbg.h> //Contains _CrtDumpMemoryLeaks()
+	#define DUMP_MEMORY_LEAKS() _CrtDumpMemoryLeaks()
+#else
+	/// @brief On Windows and Debug build, using Visual Studio, this will show memory leaks (if there are any)
+	#define DUMP_MEMORY_LEAKS() do {} while(0)
+#endif
+
 #endif //HG_COLTI_PRECOMPH
