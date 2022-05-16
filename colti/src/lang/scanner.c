dd41a867f5b99be30c3bec5d0d4afd7b08610754
R533-Code <raphaeldibnehmepc@gmail.com>
2022-05-16 19:50:41 +0300

    Added keyword and identifier parsing

--- colti/src/lang/scanner.c
+++ colti/src/lang/scanner.c
@@ -9,6 +9,7 @@ void ScannerInit(Scanner* scan, StringView to_scan)
 	colti_assert(scan != NULL, "Pointer was NULL!");
 	memset(scan, 0, sizeof(Scanner));
 	scan->view = to_scan;
+	StringInit(&scan->parsed_identifier);
 }
 
 void ScannerFree(Scanner* scan)
@@ -19,4 +20,120 @@ void ScannerFree(Scanner* scan)
 StringView ScannerGetIdentifier(const Scanner* scan)
 {
 	return StringToStringView(&scan->parsed_identifier);
+}
+
+double ScannerGetDouble(const Scanner* scan)
+{
+	return scan->parsed_double;
+}
+
+uint64_t ScannerGetInt(const Scanner* scan)
+{
+	return scan->parsed_integer;
+}
+
+Token ScannerGetNextToken(Scanner* scan)
+{
+	char next_char = impl_get_next_char(scan);
+	while (isspace(next_char))
+	{
+		if (next_char == '\n')
+			scan->current_line += 1;
+		next_char = impl_get_next_char(scan);
+	}
+
+	if (isalpha(next_char))
+		return impl_scanner_handle_identifier(scan, &next_char);
+	//else if (isdigit(next_char))
+		//return impl_scanner_handle_digit(scan, &next_char);
+	return TKN_EOF;
+}
+
+/**********************************
+IMPLEMENTATION HELPERS
+**********************************/
+
+char impl_get_next_char(Scanner* scan)
+{
+	if (scan->offset < (uint64_t)(scan->view.end - scan->view.start))
+		return scan->view.start[scan->offset++];
+	return EOF;
+}
+
+char impl_peek_next_char(Scanner* scan, uint64_t offset)
+{
+
+}
+
+Token impl_scanner_handle_identifier(Scanner* scan, char* current_char)
+{
+	//Clear the string
+	scan->parsed_identifier.size = 1;
+	scan->parsed_identifier.ptr[0] = '\0';
+
+	StringAppendChar(&scan->parsed_identifier, *current_char);
+	
+	char next_char = impl_get_next_char(scan);
+	while (isalnum(next_char))
+	{
+		StringAppendChar(&scan->parsed_identifier, next_char);
+		next_char = impl_get_next_char(scan);
+	}
+
+	//We update the current character
+	*current_char = next_char;
+	return impl_token_identifier_or_keyword(&scan->parsed_identifier);
+}
+
+Token impl_token_identifier_or_keyword(const String* string)
+{
+	const char* str = string->ptr;
+	//size - 1 as we don't care about comparing the NUL terminator
+	size_t len = string->size - 1;
+	
+	//Table of keywords
+	//We optimize comparisons by comparing the first character
+	switch (str[0])
+	{
+	break; case 'a':
+		if (strncmp(str, "and", len) == 0)
+			return TKN_OPERATOR_AND_AND;
+	break; case 'b':
+		if (strncmp(str, "break", len) == 0)
+			return TKN_KEYWORD_BREAK;
+	break; case 'c':
+		if (strncmp(str, "case", len) == 0)
+			return TKN_KEYWORD_CASE;
+		else if (strncmp(str, "continue", len) == 0)
+			return TKN_KEYWORD_CONTINUE;
+	break; case 'd':
+		if (strncmp(str, "default", len) == 0)
+			return TKN_KEYWORD_DEFAULT;
+	break; case 'e':
+		if (strncmp(str, "elif", len) == 0)
+			return TKN_KEYWORD_ELIF;
+		else if (strncmp(str, "else", len) == 0)
+			return TKN_KEYWORD_ELSE;
+	break; case 'f':
+		if (strncmp(str, "for", len) == 0)
+			return TKN_KEYWORD_FOR;
+	break; case 'g':
+		if (strncmp(str, "goto", len) == 0)
+			return TKN_KEYWORD_GOTO;
+	break; case 'i':
+		if (strncmp(str, "if", len) == 0)
+			return TKN_KEYWORD_IF;
+	break; case 'o':
+		if (strncmp(str, "or", len) == 0)
+			return TKN_OPERATOR_OR_OR;
+	break; case 's':
+		if (strncmp(str, "switch", len) == 0)
+			return TKN_KEYWORD_SWITCH;
+	break; case 'w':
+		if (strncmp(str, "while", len) == 0)
+			return TKN_KEYWORD_WHILE;
+	break; default:
+		return TKN_IDENTIFIER;
+	}
+	return TKN_IDENTIFIER;
 }
--- colti/src/lang/scanner.h
+++ colti/src/lang/scanner.h
@@ -5,13 +5,23 @@
 #include "structs/struct_string.h"
 #include "token.h"
 
+/// @brief Struct responsible of breaking a string into lexemes
 typedef struct
 {
+	/// @brief The string to scan
 	StringView view;
+	/// @brief The offset to the current character from the string
+	uint64_t offset;
+	/// @brief The offset to the beginning of the current lexeme
+	uint64_t lexeme_begin;
+	/// @brief The current line number
 	uint64_t current_line;
 
+	/// @brief The last parsed identifier
 	String parsed_identifier;
+	/// @brief The last parsed double literal
 	double parsed_double;
+	/// @brief The last parsed integer literal
 	uint64_t parsed_integer;
 } Scanner;
 
@@ -37,5 +47,37 @@ double ScannerGetDouble(const Scanner* scan);
 /// @param scan The scanner from which to get the value
 /// @return The value stored in parsed_integer
 uint64_t ScannerGetInt(const Scanner* scan);
+
+/// @brief Get the next token from a scanner.
+/// @param scan The scanner from which to get the value
+/// @return A Token representing the parsed lexeme, or TKN_EOF if there are no more lexemes
+Token ScannerGetNextToken(Scanner* scan);
+
+/**********************************
+IMPLEMENTATION HELPERS
+**********************************/
+
+/// @brief Returns the next character in the stream, and updates the offset
+/// @param scan The scanner from which to get the character
+/// @return The next character or EOF (-1) if no more characters are available
+char impl_get_next_char(Scanner* scan);
+
+/// @brief Returns the next character + offset in the stream, without updating the offset.
+/// To peek to the next character, let offset be equal to 0.
+/// @param scan The scanner from which to get the character
+/// @param offset The next character + offset
+/// @return The character or EOF if no more characters are available
+char impl_peek_next_char(Scanner* scan, uint64_t offset);
+
+/// @brief Handles an identifier case, searching for if it's a keyword or not
+/// @param scan The scanner from which to get the identifier
+/// @param current_char The pointer to the current char (which should be an alpha), which will be modified
+/// @return The Token representing the identifier
+Token impl_scanner_handle_identifier(Scanner* scan, char* current_char);
+
+/// @brief Handles comparisons for determining if an identifier is a keyword
+/// @param str The string to compare
+/// @return A Token representing a keyword, or TKN_IDENTIFIER
+Token impl_token_identifier_or_keyword(const String* string);
 
 #endif //HG_COLTI_SCANNER
