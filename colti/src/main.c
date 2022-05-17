374a30400a19860f11adb49c683353cd18a86dcb
R533-Code <raphaeldibnehmepc@gmail.com>
2022-05-17 19:45:09 +0300

    Added test code for tokens

--- colti/src/main.c
+++ colti/src/main.c
@@ -5,18 +5,31 @@ int main(int argc, const char** argv)
 	ParseResult result = ParseArguments(argc, argv);
 	if (result.file_path_in == NULL)
 	{
-		printf("> ");
-		String line = StringGetLine();
-		Scanner scan;
-		ScannerInit(&scan, StringToStringView(&line));
-		Token tkn = ScannerGetNextToken(&scan);
-		do
+		while (!feof(stdin))
 		{
-			printf("%s\n", TokenToString(tkn));
-			tkn = ScannerGetNextToken(&scan);
-		} while (tkn != TKN_EOF);
+			printf("> ");
+			String line = StringGetLine();
+			Scanner scan;
+			ScannerInit(&scan, StringToStringView(&line));
+			Token tkn = ScannerGetNextToken(&scan);
+			do
+			{
 
-		StringFree(&line);
+				switch (tkn)
+				{
+				break; case TKN_DOUBLE:
+					printf("%s: %g\n", TokenToString(tkn), scan.parsed_double);
+				break; case TKN_INTEGER:
+					printf("%s: %"PRIu64"\n", TokenToString(tkn), scan.parsed_integer);
+				break; case TKN_IDENTIFIER:
+					printf("%s: %s\n", TokenToString(tkn), scan.parsed_identifier.ptr);
+				break; default:
+					printf("%s\n", TokenToString(tkn));
+				}
+				tkn = ScannerGetNextToken(&scan);
+			} while (tkn != TKN_EOF);
+			StringFree(&line);
+		}
 	}
 	else
 	{
