1b7a78847ed9309872814d4b2335f7fbe0954a90
R533-Code <raphaeldibnehmepc@gmail.com>
2022-05-18 18:13:25 +0300

    Fixed memory leak, and added helper function

--- colti/src/main.c
+++ colti/src/main.c
@@ -1,39 +1,45 @@
 #include "precomph.h"
 
+void debug_scan(StringView view)
+{
+	Scanner scan;
+	ScannerInit(&scan, view);
+	Token tkn = ScannerGetNextToken(&scan);
+	do
+	{
+		switch (tkn)
+		{
+		break; case TKN_DOUBLE:
+			printf("%s: %g\n", TokenToString(tkn), scan.parsed_double);
+		break; case TKN_INTEGER:
+			printf("%s: %"PRIu64"\n", TokenToString(tkn), scan.parsed_uinteger);
+		break; case TKN_IDENTIFIER:
+			printf("%s: %s\n", TokenToString(tkn), scan.parsed_identifier.ptr);
+		break; default:
+			printf("%s\n", TokenToString(tkn));
+		}
+		tkn = ScannerGetNextToken(&scan);
+	} while (tkn != TKN_EOF);
+	ScannerFree(&scan);
+}
+
 int main(int argc, const char** argv)
 {
-	ParseResult result = ParseArguments(argc, argv);
-	if (result.file_path_in == NULL)
+	ParseResult args = ParseArguments(argc, argv);
+	if (args.file_path_in == NULL)
 	{
 		while (!feof(stdin))
 		{
 			printf(CONSOLE_FOREGROUND_BRIGHT_MAGENTA"> "CONSOLE_COLOR_RESET);
 			String line = StringGetLine();
-			Scanner scan;
-			ScannerInit(&scan, StringToStringView(&line));
-			Token tkn = ScannerGetNextToken(&scan);
-			do
-			{
-
-				switch (tkn)
-				{
-				break; case TKN_DOUBLE:
-					printf("%s: %g\n", TokenToString(tkn), scan.parsed_double);
-				break; case TKN_INTEGER:
-					printf("%s: %"PRIu64"\n", TokenToString(tkn), scan.parsed_uinteger);
-				break; case TKN_IDENTIFIER:
-					printf("%s: %s\n", TokenToString(tkn), scan.parsed_identifier.ptr);
-				break; default:
-					printf("%s\n", TokenToString(tkn));
-				}
-				tkn = ScannerGetNextToken(&scan);
-			} while (tkn != TKN_EOF);
+			debug_scan(StringToStringView(&line));
 			StringFree(&line);
 		}
 	}
 	else
 	{
-		String file_content = StringGetFileContent(argv[1]);
+		String file_content = StringGetFileContent(args.file_path_in);
+		debug_scan(StringToStringView(&file_content));
 		StringFree(&file_content);
 	}
 	DUMP_MEMORY_LEAKS();
